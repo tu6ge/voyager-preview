d17c3b0b9809356371c7bfd65fc8973f6ed318a5
ubuntu <ubuntu@localhost.localdomain>
2020-02-02 16:18:44 +0800

    init

---
+++ app/Providers/AppServiceProvider.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Providers;
+
+use Illuminate\Support\ServiceProvider;
+use Larapack\Hooks\Hooks;
+
+class AppServiceProvider extends ServiceProvider
+{
+    /**
+     * Register any application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        $this->app->resolving(Hooks::class, function ($api, $app) {
+            $this->app[Hooks::class]::setRemote('http://satisfy.xiaoqiezi.top');
+            $this->app[Hooks::class]->readJsonFile();
+        });
+    }
+
+    /**
+     * Bootstrap any application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+}
